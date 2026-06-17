b9a56e7d93420c48a0885ba321f1f9802e052366
Anton Teplenin <ateplenin@tehzor.ru>
2026-06-17 17:42:47 +0500

    rim-01 - Правка конфигов eslint и prettier

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,9 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import {defineConfig, globalIgnores} from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import react from 'eslint-plugin-react'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import importX from 'eslint-plugin-import-x'
 
 export default defineConfig([
     globalIgnores(['dist']),
@@ -15,10 +18,35 @@ export default defineConfig([
             tseslint.configs.recommended,
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
+            react.configs.flat.recommended,
+            jsxA11y.flatConfigs.recommended,
+            importX.flatConfigs.recommended,
+            importX.flatConfigs.typescript,
             eslintConfigPrettier,
         ],
         languageOptions: {
             globals: globals.browser,
+        },
+        settings: {
+            react: {
+                version: '19.2.6', // версия указана чтобы убрать несовместимость eslint-plugin-react с ESLint 10
+            },
+            'import-x/resolver': {
+                typescript: true,
+            },
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'import-x/order': [
+                'warn',
+                {
+                    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                    'newlines-between': 'never',
+                },
+            ],
+            'import-x/no-unresolved': 'error',
+            'import-x/no-cycle': 'warn',
+            'import-x/no-duplicates': 'warn',
         },
     },
 ])
