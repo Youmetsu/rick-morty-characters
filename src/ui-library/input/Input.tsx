192bcfba0e73d1683c4c2da90bef66aee4fa0a2e
Anton Teplenin <ateplenin@tehzor.ru>
2026-07-14 14:52:52 +0500

    rim-15 - Кастомный компонент Input

---
+++ src/ui-library/input/Input.tsx
@@ -0,0 +1,94 @@
+import {useId, useRef} from 'react'
+import classNames from 'classnames'
+import type {ChangeEvent, ReactElement} from 'react'
+import CrossIcon from '../../assets/cross-icon.svg?react'
+import './Input.css'
+
+interface InputProps {
+    value?: string
+    label?: string
+    variant?: 'underlined' | 'bordered'
+    placeholder?: string
+    renderDecoration?: () => ReactElement
+    className?: string
+    onChange?: (value: string) => void
+    dataTestIds?: {
+        label: string
+        inputContainer: string
+        input: string
+        crossIcon: string
+    }
+}
+
+export function Input({
+    value,
+    label,
+    variant = 'underlined',
+    placeholder,
+    renderDecoration,
+    className,
+    onChange,
+    dataTestIds,
+}: InputProps) {
+    const inputRef = useRef<HTMLInputElement>(null)
+    const inputId = useId()
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        onChange?.(e.currentTarget.value)
+    }
+
+    const handleClear = (): void => {
+        inputRef.current?.focus()
+        onChange?.('')
+    }
+
+    return (
+        <div className={classNames('input', className)}>
+            {label && (
+                <label
+                    htmlFor={inputId}
+                    className='input__label'
+                    data-testid={dataTestIds?.label}
+                >
+                    {label}
+                </label>
+            )}
+            <div
+                className={classNames('input__container', {
+                    'input__container--underlined': variant === 'underlined',
+                    'input__container--bordered': variant === 'bordered',
+                })}
+                data-testid={dataTestIds?.inputContainer}
+            >
+                {renderDecoration && <div className='input__decoration'>{renderDecoration()}</div>}
+                <input
+                    ref={inputRef}
+                    type='text'
+                    id={inputId}
+                    placeholder={placeholder}
+                    value={value || ''}
+                    onChange={handleChange}
+                    className={classNames('input__field', {
+                        'input__field--underlined': variant === 'underlined',
+                        'input__field--bordered': variant === 'bordered',
+                    })}
+                    data-testid={dataTestIds?.input}
+                />
+                {value && (
+                    <button
+                        type='button'
+                        aria-label='Очистить поле'
+                        className={classNames('input__clear-icon', {
+                            'input__clear-icon--underlined': variant === 'underlined',
+                            'input__clear-icon--bordered': variant === 'bordered',
+                        })}
+                        onClick={handleClear}
+                        data-testid={dataTestIds?.crossIcon}
+                    >
+                        <CrossIcon aria-hidden='true' />
+                    </button>
+                )}
+            </div>
+        </div>
+    )
+}
